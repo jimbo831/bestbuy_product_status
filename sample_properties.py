ad8d61d163a1c6afeb9f9c555a1d3640304b26cf
jimbo831 <jimbo831@gmail.com>
2017-01-14 11:58:18 -0600

    Included sample properties file.

---
+++ sample_properties.py
@@ -0,0 +1,8 @@
+# Get a Best Buy API key at https://developer.bestbuy.com
+api_key = 'your_best_buy_api_key'
+# Comma-separated list of skus in single quotes
+skus = '5577872,5621843,5578849'
+# Your zip code
+zip_code = 00000
+# Radius to search for in-store stock from your zip code in miles
+store_range = 10
